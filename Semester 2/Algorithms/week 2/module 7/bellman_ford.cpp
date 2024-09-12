9aee387503572ff5a9065316b37bd43ba31db777
Md Shagor <bci.shagor.65@gmail.com>
2024-09-12 13:15:03 +0600

    this is readme update

--- Semester 2/Algorithms/week 2/module 7/bellman_ford.cpp
+++ Semester 2/Algorithms/week 2/module 7/bellman_ford.cpp
@@ -24,14 +24,30 @@ int main()
         cin >> u >> v >> c;
         EdgeList.push_back(Edge(u, v, c));
     }
+    for (int i = 0; i < n; i++)
+    {
+        dis[i] = INT_MAX;
+    }
     // for (Edge ed : EdgeList)
     // {
     //     cout << ed.u << " " << ed.v << " " << ed.c << endl;
     // }
-    for (int i = 0; i < n; i++)
+    dis[0] = 0;
+    for (int i = 1; i <= n - 1; i++)
     {
-        dis[i] - INT_MAX;
+        for (Edge ed : EdgeList)
+        {
+            int u, v, c;
+            u = ed.u;
+            v = ed.v;
+            c = ed.c;
+            if (dis[u] < INT_MAX && dis[u] + c < dis[v])
+            {
+                dis[v] = dis[u] + c;
+            }
+        }
     }
-
+    for (int i = 0; i < n; i++)
+        cout << i << " -> " << dis[i] << endl;
     return 0;
 }
