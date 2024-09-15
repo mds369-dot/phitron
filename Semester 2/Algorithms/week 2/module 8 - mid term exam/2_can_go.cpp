6260b8be256ab931837ad42308e8859aa952cd72
Md Shagor <bci.shagor.65@gmail.com>
2024-09-15 06:18:19 +0600

    problem set 2

---
+++ Semester 2/Algorithms/week 2/module 8 - mid term exam/2_can_go.cpp
@@ -0,0 +1,147 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// const long long INF = 1e18;
+// const int N = 1005;
+// vector<pair<int, long long>> adj[N];
+// vector<long long> dist(N, INF);
+// void dijkstra(int source, int n)
+// {
+//     priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
+
+//     dist[source] = 0;
+//     pq.push({0, source});
+
+//     while (!pq.empty())
+//     {
+//         pair<long long, int> p = pq.top();
+//         long long d = p.first;
+//         int u = p.second;
+//         pq.pop();
+
+//         if (d > dist[u])
+//             continue;
+
+//         for (auto &edge : adj[u])
+//         {
+//             int v = edge.first;
+//             long long w = edge.second;
+//             if (dist[u] + w < dist[v])
+//             {
+//                 dist[v] = dist[u] + w;
+//                 pq.push({dist[v], v});
+//             }
+//         }
+//     }
+// }
+
+// int main()
+// {
+//     int n, e;
+//     cin >> n >> e;
+//     for (int i = 0; i < e; i++)
+//     {
+//         int a, b;
+//         long long w;
+//         cin >> a >> b >> w;
+//         adj[a].push_back({b, w});
+//     }
+
+//     int source;
+//     cin >> source;
+//     dijkstra(source, n);
+
+//     int t;
+//     cin >> t;
+//     while (t--)
+//     {
+//         int dest;
+//         long long maxCost;
+//         cin >> dest >> maxCost;
+
+//         if (dist[dest] <= maxCost)
+//         {
+//             cout << "YES" << endl;
+//         }
+//         else
+//         {
+//             cout << "NO" << endl;
+//         }
+//     }
+
+//     return 0;
+// }
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const long long INF = 1e18;
+const int N = 1005;
+vector<pair<int, long long>> adj[N];
+vector<long long> dist(N, INF);
+vector<bool> visited(N, false);
+void bfs_dijkstra(int source, int n)
+{
+    queue<int> q;
+
+    dist[source] = 0;
+    q.push(source);
+
+    while (!q.empty())
+    {
+        int u = q.front();
+        q.pop();
+
+        if (visited[u])
+            continue;
+        visited[u] = true;
+
+        for (auto &edge : adj[u])
+        {
+            int v = edge.first;
+            long long w = edge.second;
+            if (dist[u] + w < dist[v])
+            {
+                dist[v] = dist[u] + w;
+                q.push(v);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    for (int i = 0; i < e; i++)
+    {
+        int a, b;
+        long long w;
+        cin >> a >> b >> w;
+        adj[a].push_back({b, w});
+    }
+
+    int source;
+    cin >> source;
+    bfs_dijkstra(source, n);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int dest;
+        long long maxCost;
+        cin >> dest >> maxCost;
+
+        if (dist[dest] <= maxCost)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
