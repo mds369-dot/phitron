9d3f5d7e4ab3791b8e2de1316cec8c2982b2d184
Md Shagor <bci.shagor.65@gmail.com>
2024-09-15 06:21:08 +0600

    problem set 2 enhance

--- Semester 2/Algorithms/week 2/module 8 - mid term exam/2_can_go.cpp
+++ Semester 2/Algorithms/week 2/module 8 - mid term exam/2_can_go.cpp
@@ -72,38 +72,99 @@
 //     return 0;
 // }
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// const long long INF = 1e18;
+// const int N = 1005;
+// vector<pair<int, long long>> adj[N];
+// vector<long long> dist(N, INF);
+// vector<bool> visited(N, false);
+// void bfs_dijkstra(int source, int n)
+// {
+//     queue<int> q;
+
+//     dist[source] = 0;
+//     q.push(source);
+
+//     while (!q.empty())
+//     {
+//         int u = q.front();
+//         q.pop();
+
+//         if (visited[u])
+//             continue;
+//         visited[u] = true;
+
+//         for (auto &edge : adj[u])
+//         {
+//             int v = edge.first;
+//             long long w = edge.second;
+//             if (dist[u] + w < dist[v])
+//             {
+//                 dist[v] = dist[u] + w;
+//                 q.push(v);
+//             }
+//         }
+//     }
+// }
+
+// int main()
+// {
+//     int n, e;
+//     cin >> n >> e;
+//     for (int i = 0; i < e; i++)
+//     {
+//         int a, b;
+//         long long w;
+//         cin >> a >> b >> w;
+//         adj[a].push_back({b, w});
+//     }
+
+//     int source;
+//     cin >> source;
+//     bfs_dijkstra(source, n);
+
+//     int t;
+//     cin >> t;
+//     while (t--)
+//     {
+//         int dest;
+//         long long maxCost;
+//         cin >> dest >> maxCost;
+
+//         if (dist[dest] <= maxCost)
+//         {
+//             cout << "YES" << endl;
+//         }
+//         else
+//         {
+//             cout << "NO" << endl;
+//         }
+//     }
+
+//     return 0;
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
 
 const long long INF = 1e18;
 const int N = 1005;
-vector<pair<int, long long>> adj[N];
+vector<tuple<int, int, long long>> edges;
 vector<long long> dist(N, INF);
-vector<bool> visited(N, false);
-void bfs_dijkstra(int source, int n)
+void bellman_ford(int source, int n)
 {
-    queue<int> q;
-
     dist[source] = 0;
-    q.push(source);
 
-    while (!q.empty())
+    // Relax edges up to (n-1) times
+    for (int i = 1; i <= n - 1; i++)
     {
-        int u = q.front();
-        q.pop();
-
-        if (visited[u])
-            continue;
-        visited[u] = true;
-
-        for (auto &edge : adj[u])
+        for (auto [u, v, w] : edges)
         {
-            int v = edge.first;
-            long long w = edge.second;
-            if (dist[u] + w < dist[v])
+            if (dist[u] < INF && dist[u] + w < dist[v])
             {
                 dist[v] = dist[u] + w;
-                q.push(v);
             }
         }
     }
@@ -113,20 +174,23 @@ int main()
 {
     int n, e;
     cin >> n >> e;
+
     for (int i = 0; i < e; i++)
     {
         int a, b;
         long long w;
         cin >> a >> b >> w;
-        adj[a].push_back({b, w});
+        edges.push_back(make_tuple(a, b, w));
     }
 
     int source;
     cin >> source;
-    bfs_dijkstra(source, n);
+
+    bellman_ford(source, n);
 
     int t;
     cin >> t;
+
     while (t--)
     {
         int dest;
