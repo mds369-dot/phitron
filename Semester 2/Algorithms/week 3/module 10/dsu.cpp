c35963d722c3dc747f4ed90d21ee3c0ae131e7ed
Md Shagor <bci.shagor.65@gmail.com>
2024-09-17 05:49:51 +0600

    dsu union and union by size readme update and the code too

--- Semester 2/Algorithms/week 3/module 10/dsu.cpp
+++ Semester 2/Algorithms/week 3/module 10/dsu.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 const int N = 1e5 + 5;
 int par[N];
-int groudp_size[N];
+int group_size[N];
 void dsu_initialize(int n)
 {
     for (int i = 0; i < n; i++)
     {
         par[i] = -1;
-        groudp_size[i] = 1;
+        group_size[i] = 1;
     }
     // par[1] = 3;
     // par[2] = 1;
@@ -33,15 +33,15 @@ void dsu_union_by_size(int node1, int node2)
 {
     int leaderA = dsu_find(node1);
     int leaderB = dsu_find(node2);
-    if (groudp_size[leaderA] > groudp_size[leaderB])
+    if (group_size[leaderA] > group_size[leaderB])
     {
         par[leaderB] = leaderA;
-        groudp_size[leaderA] += groudp_size[leaderB];
+        group_size[leaderA] += group_size[leaderB];
     }
     else
     {
         par[leaderA] = leaderB;
-        groudp_size[leaderB] += groudp_size[leaderA];
+        group_size[leaderB] += group_size[leaderA];
     }
 }
 int main()
