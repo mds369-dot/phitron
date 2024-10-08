1dc86bca7bc625893b2bf2e553615ced5afaedd2
Md Shagor <bci.shagor.65@gmail.com>
2024-10-08 13:52:31 +0600

    fibonacci code upload of optimized

--- Semester 2/Algorithms/week 5/module 16/febonacci.cpp
+++ Semester 2/Algorithms/week 5/module 16/febonacci.cpp
@@ -1,18 +1,27 @@
 #include <bits/stdc++.h>
+#define ll long long
 using namespace std;
-int fibo(int n)
+const ll N = 1e6 + 5;
+ll dp[N];
+ll fibo(ll n)
 {
     if (n == 0 || n == 1)
     {
         return n;
     }
-    int ans = fibo(n - 1) + fibo(n - 2);
+    if (dp[n] != -1)
+    {
+        return dp[n];
+    }
+    ll ans = fibo(n - 1) + fibo(n - 2);
+    dp[n] = ans;
     return ans;
 }
 int main()
 {
-    int n;
+    ll n;
     cin >> n;
+    memset(dp, -1, sizeof(dp));
     cout << fibo(n) << endl;
     return 0;
 }
