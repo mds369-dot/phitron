b02e6a23b759f0de7cd15454779ed8432865c829
mds369-dot <mdshagor369@vzm7w.onmicrosoft.com>
2025-01-02 07:06:23 +0600

    just javascript simple file

---
+++ Semester 3/Sotware Development Project/week 2/module 5/app.js
@@ -0,0 +1,70 @@
+// var test = 'Hello World';
+
+// var num = 10;
+
+// var status = true;
+
+// console.log(typeof status)
+
+
+// var obj = {
+//     num : "gias",
+// };
+
+// console.log(typeof obj)
+
+var num1 = "12.5"
+var num2 = 15;
+// var converedNum = parseFloat(num1)
+
+var result = parseFloat(num1) + num2
+
+// console.log(typeof result)
+
+// var status = "rain";
+// var status = "sun shine";
+
+// if ("rain" == status){
+//     console.log("baire jawa jabe nah");
+// }
+// else if("sun shine" == status){
+//     console.log("bahare gala, chata niya jaitay hobo")
+// }
+// else{
+//     console.log("baire jawa jabe")
+// }
+
+// 0 - 39 = C
+// 40 - 69 = B
+// 60 - 69 = A-
+// 70 - 79 = A
+// 80 - 100 = A+
+
+// var result = -1;
+
+// if(result < 0){
+//     console.log("Failed")
+// }
+// else if(result >= 0 && result <= 39){
+//     console.log("C")
+// }
+// else if(result >= 40 && result <= 69){
+//     console.log("B")
+// }
+// else{
+//     console.log("invalid")
+// }
+
+var result = 80
+
+var person = {
+    hand: 2,
+    eye: 2,
+    hair: "Don't Know",
+    father: {
+        name: "John",
+        age: 20,
+    },
+}
+
+console.log(person.father.age)
