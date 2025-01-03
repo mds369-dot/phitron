38439383a86da8ed14a543170d29f079480b47f6
Md Shagor(Musa daNyal Shagor) <mdshagor.three.six.nine@gmail.com>
2025-01-03 11:49:41 +0600

    Javascript and DOM

---
+++ Semester 3/Sotware Development Project/week 2/module 6/app.js
@@ -0,0 +1,37 @@
+// const num = 5;
+// num = 50;
+// let name = "GIAS";
+
+// name = "Shagor";
+// name = "Ujjol";
+
+// console.log(num)
+// console.log(name)
+
+// const countryName = "Bangladesh";
+
+// const country = ` My country is ${countryName}`;
+
+// console.log(country)
+
+// const number = [1,2,3,4,5,6,7,8,9,10];
+
+// const number2 = [11, 12, 13, 14, 15, 16, 17, 18, 19];
+
+// console.log(...number, ...number2);
+
+// const person = {
+//     name: "test",
+//     age: 10,
+//     friends: ["karim", "rohim","jabbar"],
+// };
+
+// const {age, friends} = person;
+
+// console.log(friends)
+
+const names = ["korim", 10, "lorem ifui slsdf dfu"];
+
+const [name, age, descrip] = names; 
+
+console.log(name, age, descrip)
