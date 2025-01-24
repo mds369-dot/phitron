43117b008ced83391e0fa446314d6c6bcb5f7239
Md Shagor <mdshagor369@vzm7w.onmicrosoft.com>
2025-01-24 22:43:20 +0600

    django learning

--- Semester 3/Milestone 1 - Getting Started With Django/module 4/tasks/views.py
+++ Semester 3/Milestone 1 - Getting Started With Django/module 4/tasks/views.py
@@ -3,7 +3,14 @@ from django.http import HttpResponse
 # Create your views here.
 
 def manager_dashboard(request):
-    return render(request, 'dashboard.html')
+    return render(request, 'dashboard/manager-deshboard.html')
 def user_dashboard(request):
-    return render(request, 'user-dashboard.html')
+    return render(request, 'dashboard/user-dashboard.html')
+
+def test(request):
+    context = {
+        "names" : ["Rahul", "Rohit", "Rajesh", "Ramesh", "John"],
+        "age" : [20, 21, 22, 23],
+    }
+    return render(request, 'test.html', context)
 
