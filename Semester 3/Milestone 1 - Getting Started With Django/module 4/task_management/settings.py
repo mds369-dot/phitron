c465ee1797f7a4c55396686e6511da55388ba1c8
Md Shagor <mdshagor369@vzm7w.onmicrosoft.com>
2025-01-25 12:01:46 +0600

    this is the postgeSQL halfPart

--- Semester 3/Milestone 1 - Getting Started With Django/module 4/task_management/settings.py
+++ Semester 3/Milestone 1 - Getting Started With Django/module 4/task_management/settings.py
@@ -75,12 +75,26 @@ WSGI_APPLICATION = 'task_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+#for sqlite3
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+#for postgresql
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'task_management',
+        'USER': 'postgres',
+        'PASSWORD': '144950',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
+
 
 
 # Password validation
--- Semester 3/Milestone 1 - Getting Started With Django/module 4/tasks/models.py
+++ Semester 3/Milestone 1 - Getting Started With Django/module 4/tasks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class task(models.Model):
+   title = models.CharField(max_length=250)
+   description = models.TextField()
+   due_date = models.DateField()
+   is_completed = models.BooleanField(default=False)
+   created_at = models.DateTimeField(auto_now_add=True)
+   updated_at = models.DateTimeField(auto_now=True)
+
+
+   # One to One
+   # Many to One
+   # Many to Many
