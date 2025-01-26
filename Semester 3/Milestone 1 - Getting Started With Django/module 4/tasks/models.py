76953219033bc758b30417a4d174689a31124690
Md Shagor <mdshagor369@vzm7w.onmicrosoft.com>
2025-01-26 13:18:17 +0600

    this is module 5 half and 6 some

--- Semester 3/Milestone 1 - Getting Started With Django/module 4/tasks/models.py
+++ Semester 3/Milestone 1 - Getting Started With Django/module 4/tasks/models.py
@@ -1,15 +1,45 @@
 from django.db import models
 
 # Create your models here.
-class task(models.Model):
-   title = models.CharField(max_length=250)
-   description = models.TextField()
-   due_date = models.DateField()
-   is_completed = models.BooleanField(default=False)
-   created_at = models.DateTimeField(auto_now_add=True)
-   updated_at = models.DateTimeField(auto_now=True)
 
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
 
-   # One to One
-   # Many to One
-   # Many to Many
+class Task(models.Model):
+    project = models.ForeignKey("Project", on_delete=models.CASCADE, default=1)
+    assigned_to = models.ManyToManyField(Employee)
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    due_date = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # One to One
+    # Many to One
+    # Many to Many
+
+class TaskDetail(models.Model):
+    HIGH = 'H'
+    MEDIUM = 'M'
+    LOW = 'L'
+    priority_options = [
+        (HIGH, 'High'),
+        (MEDIUM, 'Medium'),
+        (LOW, 'Low')
+    ]
+    task = models.OneToOneField(Task, on_delete=models.CASCADE)
+    assigned_to = models.CharField(max_length=100)
+    priority = models.CharField(max_length=1, choices=priority_options, default=LOW)
+
+# task.object.get(id=2)
+# select * from task where id=2
+# ORM
+
+class Project(models.Model):
+   name = models.CharField(max_length=250)
+   start_date = models.DateField()
+
+# task = onekgula employee ekta task
+# employee = onekgula task ar jono assign kora asay
