5e975e549ce3c05a25d7a77facdc11a07329683f
Md Shagor <mdshagor369@vzm7w.onmicrosoft.com>
2025-01-27 11:49:38 +0600

    restart

--- Semester 3/Milestone 1 - Getting Started With Django/module 4/tasks/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from tasks.views import manager_dashboard, user_dashboard, test
-
-urlpatterns = [
-    path('manager_dashboard/', manager_dashboard),
-    path('user_dashboard/', user_dashboard),
-    path('test/', test)
-]
